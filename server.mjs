9ba47df7054a6c8bccac3bcb7953f3a8ba47f176
amanverma.verma530@gmail.com <amanverma.verma530@gmail.com>
2025-06-05 11:25:00 +0530

    colors package added

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import connectDB from './config/db.mjs';
 //load env vars
 dotenv.config({ path: './config/config.env' });
@@ -19,12 +20,15 @@ app.use('/app/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server is running ${process.env.NODE_ENV} on PORT ${PORT}`);
+  console.log(
+    `Server is running ${process.env.NODE_ENV} on PORT ${PORT}`.brightYellow
+      .bold
+  );
 });
 
 //handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   server.close(() => {
     //Close server and exit process
     process.exit(1);
