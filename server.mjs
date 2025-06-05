4bb1f50192439f5021aad5a48f5b9f9eba9670a7
amanverma.verma530@gmail.com <amanverma.verma530@gmail.com>
2025-06-05 11:17:52 +0530

    monogdb database connection added

--- server.mjs
+++ server.mjs
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.mjs';
+//load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 //route files
 import bootcamps from './routes/bootcamps.mjs';
-
-//load env vars
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
 app.use(express.json());
@@ -15,6 +18,15 @@ app.use('/app/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running ${process.env.NODE_ENV} on PORT ${PORT}`);
+});
+
+//handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => {
+    //Close server and exit process
+    process.exit(1);
+  });
 });
