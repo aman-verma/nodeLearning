3049bf24d5a0ffcd0b65c3b6879650ca90fb23c1
amanverma.verma530@gmail.com <amanverma.verma530@gmail.com>
2025-06-10 19:54:27 +0530

    vercel config added 3

--- server.mjs
+++ server.mjs
@@ -17,6 +17,10 @@ app.use(express.json());
 //mount routes
 app.use('/app/v1/bootcamps', bootcamps);
 
+app.get('/', (req, res) => {
+  res.send('vercel working');
+});
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
